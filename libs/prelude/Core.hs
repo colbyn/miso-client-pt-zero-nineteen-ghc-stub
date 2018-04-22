932facfdbe88eb0f38bfa084740c9bf58a8f17e6
Colbyn Wadman <hello@colbyn.com>
2018-04-21 20:37:14 -0600

    Readme Update

--- libs/prelude/Core.hs
+++ libs/prelude/Core.hs
@@ -4,6 +4,7 @@ module Core (
     , module Proto.Class
     , module Proto.Util
     , module Proto.System.Console
+    , stub
 )
 where
 
@@ -12,5 +13,11 @@ import Proto.Data
 import Proto.Util
 import Proto.Class
 import Proto.System.Console
+import qualified Prelude
+
+
+-- For Stubs
+stub :: a
+stub = Prelude.error "Attempted evaluation of a ghc-js stub."
 
 
